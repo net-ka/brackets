0ef7c39b85631a41a3a759d59ee67476895c74a1
net-ka <a.k.fedosova@gmail.com>
2019-03-14 03:15:08 +0200

    brackets: solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+  let strArray = str.split('');
+  let stack = [strArray[0]];
+
+  for (i = 1; i < strArray.length; i++) {
+    stack.push(strArray[i]);
+    for (let k = 0; k < bracketsConfig.length; k++) {
+      if (bracketsConfig[k][0] == stack[stack.length - 2] && bracketsConfig[k][1] == stack[stack.length - 1]) {
+        stack.splice(stack.length - 2, 2);
+      }
+    } 
+  }
+
+  if (stack.length == 0) {
+    return true;
+  } else {
+    return false;
+  }
 }
